9d9fabc977b7b63611ca41fb3bdcdbe336a4f8f4
Daniel Hernandez <danielmark252525@GMAIL.COM>
2020-04-10 07:27:45 -0600

    Add api call to make app

--- client/src/Components/AppSettings/AppSettings.jsx
+++ client/src/Components/AppSettings/AppSettings.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React,{useState} from "react";
+import {useEditor} from  "@craftjs/core"
+
+import {makeApp} from "../../utils/apiCalls"
 
 
 import "./AppSettings.scss"
 
 const AppSettings = () => {
+    const [componentName, setComponentName] = useState("")
+    const [applicationName, setApplicationName] = useState("")
+
+    const {query} = useEditor();
+    const jsonState = () => query.serialize()
+
+
+    const handleAppServerCall = async () =>{
+        let data = await makeApp(componentName,applicationName,jsonState())
+    }
     return(
         <div className="parse">
-            <input type="text" name="" id=""/>
-            <input type="text" name="" id=""/>
+            <input type="text" value={componentName} onChange={(e) => setComponentName(e.target.value)}/>
+            <input type="text" value={applicationName} onChange={(e) => setApplicationName(e.target.value)}/>
+            <input onClick={handleAppServerCall} type="submit"value="Make Applaction"/>
         </div>
     )
 };
