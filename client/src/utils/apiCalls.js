d13d1f633c63928668f946af401df992981a0481
Daniel Hernandez <danielmark252525@GMAIL.COM>
2020-04-26 12:45:07 -0600

    Add formating

--- client/src/utils/apiCalls.js
+++ client/src/utils/apiCalls.js
@@ -1,15 +1,15 @@
-import axios from "axios"
+import axios from "axios";
 
 export const makeApp = (applicationName) => {
-    const apiEndPoint = "http://localhost:5000/makeApplication"
-    const params ={applicationName}
-    const serverData = axios.post(apiEndPoint,params)
-    return serverData
-}
+  const apiEndPoint = "http://localhost:5000/makeApplication";
+  const params = { applicationName };
+  const serverData = axios.post(apiEndPoint, params);
+  return serverData;
+};
 
-export const makeComponent = (componentName,applicationName,jsonData) => {
-    const apiEndPoint = "http://localhost:5000/makeComponent"
-    const params ={componentName,applicationName,jsonData}
-    const serverData = axios.post(apiEndPoint,params)
-    return serverData
-}
+export const makeComponent = (componentName, applicationName, jsonData) => {
+  const apiEndPoint = "http://localhost:5000/makeComponent";
+  const params = { componentName, applicationName, jsonData };
+  const serverData = axios.post(apiEndPoint, params);
+  return serverData;
+};
